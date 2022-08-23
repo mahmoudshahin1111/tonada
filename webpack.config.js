c4ed4d8b99fbd9a0353896a773b751dcd07ae15f
Mahmoud shahin <mahmoudshahin1111@gmail.com>
2022-08-23 20:50:23 +0200

    splitted to components and can import only your needs (#8)

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,48 @@
-const path = require('path');
+const path = require("path");
+const package = require("./package.json");
 
-module.exports = {
-  entry: './src/ts/index.ts',
+const sharedConfig = {
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
-  output: {
-    filename: 'tonada.js', 
-    path: path.resolve(__dirname, 'dist','js'),
-    library:{
-      name:'Tornada',
-      type:'window'
-    }
-  },
-  
 };
+
+module.exports = [
+  ...package.components.map((component) => {
+    return {
+      ...sharedConfig,
+      entry: {
+        [component.name]: path.resolve(__dirname, component.path),
+      },
+      output: {
+        path: path.resolve(__dirname, "dist", "js"),
+        library: {
+          name: component.className,
+          type: "window",
+        },
+      },
+    };
+  }),
+  {
+    ...sharedConfig,
+    entry: {
+      index: path.resolve(__dirname, "src/ts", "index.ts"),
+    },
+    output: {
+      path: path.resolve(__dirname, "dist", "js"),
+      library: {
+        name: "Tonada",
+        type: "window",
+      },
+    },
+  },
+];
